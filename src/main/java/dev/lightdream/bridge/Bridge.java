6dd7fff9c8340f8b91ff9deb308aad64d4a7f986
L1ghtDream <62599375+L1ghtDream@users.noreply.github.com>
2021-03-06 19:50:13 +0200

    Initial Commit

---
+++ src/main/java/dev/lightdream/bridge/Bridge.java
@@ -0,0 +1,17 @@
+package dev.lightdream.bridge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bridge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
